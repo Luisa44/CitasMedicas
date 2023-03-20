16ae6dfa990d9b7a0b2e2cd3994467f3e120a69b
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2023-03-19 22:38:16 -0500

    account-validation view added

--- app/lib/features/auth/account-validation/account_validation.view.dart
+++ app/lib/features/auth/account-validation/account_validation.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../login/login.view.dart';
 
 class AccountValidationPage extends StatefulWidget {
   const AccountValidationPage({super.key});
@@ -10,8 +11,75 @@ class AccountValidationPage extends StatefulWidget {
 }
 
 class _AccountValidationPage extends State<AccountValidationPage> {
+  bool _isObscurePassword = true;
+  bool _isObscureConPassword = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Account Validation Page'));
+    return Scaffold(
+        body: Container(
+      margin: const EdgeInsets.only(left: 30, right: 30, top: 80, bottom: 50),
+      child: Column(children: [
+        const Text('Cambio de Contraseña',
+            style: TextStyle(color: Colors.blue, fontSize: 30)),
+        const SizedBox(height: 50),
+        TextField(
+          obscureText: _isObscurePassword,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              hintText: 'Nueva Contraseña',
+              suffixIcon: IconButton(
+                icon: Icon(_isObscurePassword
+                    ? Icons.visibility
+                    : Icons.visibility_off),
+                onPressed: () => {
+                  setState(() {
+                    _isObscurePassword = !_isObscurePassword;
+                  })
+                },
+              )),
+        ),
+        const SizedBox(height: 15),
+        TextField(
+          obscureText: _isObscureConPassword,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              hintText: 'Confirma Contraseña',
+              suffixIcon: IconButton(
+                icon: Icon(_isObscureConPassword
+                    ? Icons.visibility
+                    : Icons.visibility_off),
+                onPressed: () => {
+                  setState(() {
+                    _isObscureConPassword = !_isObscureConPassword;
+                  })
+                },
+              )),
+        ),
+        const SizedBox(height: 40),
+        ElevatedButton(
+            onPressed: () => {},
+            style: OutlinedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            child: Text('Guardar')),
+        const SizedBox(height: 15),
+        ElevatedButton(
+            onPressed: () => {
+              Navigator.pushNamed(context, LoginPage.routeName)
+            },
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.red,
+              minimumSize: const Size.fromHeight(50),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            child: Text('Cancelar')),
+      ]),
+    ));
   }
 }
