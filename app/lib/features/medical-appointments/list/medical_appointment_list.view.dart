0fda3f74e39f9be966bb082d0f653665d94c3bca
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2023-03-19 22:40:06 -0500

    medical appointments added

--- app/lib/features/medical-appointments/list/medical_appointment_list.view.dart
+++ app/lib/features/medical-appointments/list/medical_appointment_list.view.dart
@@ -10,8 +10,80 @@ class MedicalAppointmentListPage extends StatefulWidget {
 }
 
 class _MedicalAppointmentListPage extends State<MedicalAppointmentListPage> {
+  List<String> items = List<String>.generate(15, (i) => 'item ${i}');
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Text('Medical Appointment List Page'));
+    return Scaffold(
+      appBar: AppBar(title: Text('Citas Medicas')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Header')
+            ),
+            ListTile(
+              title: const Text('Citas Medicas'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Programar Cita'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Historial de Citas'),
+              onTap: () {},
+            ),
+          ],
+        )
+      ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context,index){
+            return Card(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text('Title ${index}'),
+                    subtitle: Text('description ${index}'),
+                    leading: Icon(Icons.calendar_today_rounded),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => {}, 
+                        child: Icon(Icons.remove_red_eye),
+                        style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)
+                          )
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      ElevatedButton(
+                        onPressed: () => {}, 
+                        child: Icon(Icons.delete),
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)
+                          )
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                    ]
+                  ),
+                  const SizedBox(height: 5)
+                ]
+              ),
+            );
+          }
+        ),
+    );
   }
 }
