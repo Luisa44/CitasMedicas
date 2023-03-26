225d8231eb02c38b1c652b76bb35323f84d3150a
Stiven Gaitan <johan.gaitan@mqa-bc.com>
2023-03-26 17:38:45 -0500

    users list view was added

--- app/lib/features/users/list/user_list.view.dart
+++ app/lib/features/users/list/user_list.view.dart
@@ -24,183 +24,156 @@ class _UserListPage extends State<UserListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Usuarios')),
-        drawer: Drawer(
-            child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text('Header')),
-          ],
-        )),
-        body: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
-                child: Row(
-                  children: [
-                    // Imagen del usuario en la parte derecha del card
-                    const SizedBox(width: 15),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: 100.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: items[index].role == 'Doctor'
-                                ? AssetImage('assets/images/doctor_profile.png')
-                                : AssetImage('assets/images/user_men.png'),
-                            fit: BoxFit.cover,
+      appBar: AppBar(title: Text('Usuarios')),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Header')),
+        ],
+      )),
+      body: Column(children: [
+        const SizedBox(height: 7),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Buscar',
+            prefixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Expanded(
+            child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    child: Row(
+                      children: [
+                        // Imagen del usuario en la parte derecha del card
+                        const SizedBox(width: 15),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: 100.0,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: items[index].role == 'Doctor'
+                                    ? AssetImage(
+                                        'assets/images/doctor_profile.png')
+                                    : AssetImage('assets/images/user_men.png'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    // Información del usuario en la parte izquierda del card
-                    Expanded(
-                      flex: 3,
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(0.0, 16.0, 16.0, 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(height: 30),
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 95, 95, 95)),
-                                children: [
-                                  const TextSpan(
-                                      text: 'Nombre: ',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                  TextSpan(
-                                      text:
-                                          '${items[index].name} ${items[index].surname}'),
-                                ],
-                              ),
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 95, 95, 95)),
-                                children: [
-                                  const TextSpan(
-                                      text: 'Rol: ',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                  TextSpan(text: items[index].role),
-                                ],
-                              ),
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 95, 95, 95)),
-                                children: [
-                                  const TextSpan(
-                                      text: 'Documento: ',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold)),
-                                  TextSpan(text: items[index].document),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 16.0),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                        const SizedBox(width: 16.0),
+                        // Información del usuario en la parte izquierda del card
+                        Expanded(
+                          flex: 3,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                0.0, 16.0, 16.0, 16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                ElevatedButton(
-                                  onPressed: () => {},
-                                  child: Row(children: [
-                                    Text('Detalle'),
-                                    const SizedBox(width: 5.0),
-                                    Icon(Icons.remove_red_eye)
-                                  ]),
-                                  style: OutlinedButton.styleFrom(
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(12))),
+                                const SizedBox(height: 30),
+                                RichText(
+                                  text: TextSpan(
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 95, 95, 95)),
+                                    children: [
+                                      const TextSpan(
+                                          text: 'Nombre: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      TextSpan(
+                                          text:
+                                              '${items[index].name} ${items[index].surname}'),
+                                    ],
+                                  ),
                                 ),
-                                const SizedBox(width: 8),
-                                ElevatedButton(
-                                  onPressed: () => {},
-                                  child: Row(children: [
-                                    Text('Eliminar'),
-                                    const SizedBox(width: 5.0),
-                                    Icon(Icons.delete)
-                                  ]),
-                                  style: OutlinedButton.styleFrom(
-                                      backgroundColor: Colors.red,
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(12))),
+                                RichText(
+                                  text: TextSpan(
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 95, 95, 95)),
+                                    children: [
+                                      const TextSpan(
+                                          text: 'Rol: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      TextSpan(text: items[index].role),
+                                    ],
+                                  ),
+                                ),
+                                RichText(
+                                  text: TextSpan(
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 95, 95, 95)),
+                                    children: [
+                                      const TextSpan(
+                                          text: 'Documento: ',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      TextSpan(text: items[index].document),
+                                    ],
+                                  ),
+                                ),
+                                SizedBox(height: 16.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: () => {},
+                                      child: Row(children: [
+                                        Text('Detalle'),
+                                        const SizedBox(width: 5.0),
+                                        Icon(Icons.remove_red_eye)
+                                      ]),
+                                      style: OutlinedButton.styleFrom(
+                                          backgroundColor: Color.fromRGBO(
+                                              47, 137, 255, 0.612),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12))),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    ElevatedButton(
+                                      onPressed: () => {},
+                                      child: Row(children: [
+                                        Text('Eliminar'),
+                                        const SizedBox(width: 5.0),
+                                        Icon(Icons.delete)
+                                      ]),
+                                      style: OutlinedButton.styleFrom(
+                                          minimumSize: const Size.fromHeight(45),
+                                          backgroundColor: Color.fromRGBO(
+                                              255, 47, 47, 0.612),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12))),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
-              );
-
-              /*Card(
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Text('Title ${index}'),
-                    subtitle: Text('description ${index}'),
-                    leading: items[index].role == 'Doctor' ? 
-                    Container(
-                      width: 80,
-                      height: 80,
-                      child: Image.asset('assets/images/doctor_profile.png', fit: BoxFit.cover),
-                    ) : 
-                    Container(
-                      width: 150,
-                      height: 150,
-                      child: Image.asset('assets/images/user_men.png', fit: BoxFit.cover),
-                    )
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () => {}, 
-                        child: Icon(Icons.remove_red_eye),
-                        style: OutlinedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)
-                          )
-                        ),
-                      ),
-                      const SizedBox(width: 5),
-                      ElevatedButton(
-                        onPressed: () => {}, 
-                        child: Icon(Icons.delete),
-                        style: OutlinedButton.styleFrom(
-                          backgroundColor: Colors.red,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)
-                          )
-                        ),
-                      ),
-                      const SizedBox(width: 20),
-                    ]
-                  ),
-                  const SizedBox(height: 5)
-                ]
-              ),
-            );*/
-            }));
+                  );
+                }))
+      ]),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.add)),
+    );
   }
 }
